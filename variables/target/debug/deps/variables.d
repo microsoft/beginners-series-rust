c4afa9b5ac0c82dc4329ae87909ec6945233fd4a
meaghanlewis <molewis@microsoft.com>
2021-04-14 16:01:40 -0700

    remove target directory

--- variables/target/debug/deps/variables.d
+++
@@ -1,5 +0,0 @@
-/Users/meaghanlewis/Projects/beginners-series-rust/variables/target/debug/deps/variables: src/main.rs
-
-/Users/meaghanlewis/Projects/beginners-series-rust/variables/target/debug/deps/variables.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/meaghanlewis/Projects/beginners-series-rust/variables/target/debug/variables: /Users/meaghanlewis/Projects/beginners-series-rust/variables/src/main.rs
